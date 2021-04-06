69c8515fbae1d8696e559b5ff37fc72243490914
gitlinhleqtgh <linhleqtgh@gmail.com>
2021-04-07 01:46:31 +0700

    add product to cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   include StoresHelper
+  before_action :set_cart
+
+  private
+
+  def set_cart
+    unless session[:cart]
+      session[:cart] = []
+      @cart = session[:cart]
+    end
+    @cart = session[:cart]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'carts/index'
   root "stores#index"
   get "stores/detail/:id", to: "stores#detail", as: "detail"
+  get "carts/:id", to: "carts#index", as: "carts"
 end
